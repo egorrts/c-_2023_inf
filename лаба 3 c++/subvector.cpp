17da7566615a2c8183d4aac141d9e03b2f2cf1c9
egorrts <igortsaturyan0612@gmail.com>
2023-05-10 06:08:10 +0300

    completed subvector

--- лаба 3 c++/subvector.cpp
+++ лаба 3 c++/subvector.cpp
@@ -1,3 +1,4 @@
+//при загрузке в lms убираем этот фрагмент
 struct subvector {
     int *mas; //указателя на массив в куче
     unsigned int top; //размер той памяти, с которой мы работаем, в которой есть осмысленные данные
@@ -9,7 +10,28 @@ bool init (subvector *qv) {
     qv->capacity = 0;
     qv->top = 0;
     qv->mas = NULL;
+    return true;
 } 
+
+//увеличить емкость недовектора (можно использовать и для уменьшения - тогда, в рамках данной реализации, если top меньше новой capacity, то копируем только то, что влезает, и уменьшаем top до capacity)
+bool resize (subvector *qv, unsigned int new_capacity) {
+    int* a = new int[new_capacity];
+    if (new_capacity >= qv->capacity){
+        for (unsigned int i = 0; i < qv->top; i++){
+            a[i] = (qv->mas)[i];
+        }
+    } 
+    else {
+    for (unsigned int i = 0; i < new_capacity; ++i) {
+        a[i] = qv->mas[i];
+    }
+    qv->top = new_capacity;
+    }
+    qv->capacity = new_capacity;
+    delete[] qv->mas;
+    qv->mas = a;
+    return true;
+}
 
 // добавление элемента в конец недовектора с выделением дополнительной памяти при необходимости
 bool push_back (subvector *qv, int d) {
@@ -36,26 +58,6 @@ int pop_back (subvector *qv) {
     }
     return 0;
 } 
-
-//увеличить емкость недовектора (можно использовать и для уменьшения - тогда, в рамках данной реализации, если top меньше новой capacity, то копируем только то, что влезает, и уменьшаем top до capacity)
-bool resize (subvector *qv, unsigned int new_capacity) {
-    int* a = new int[new_capacity];
-    if (new_capacity >= qv->capacity){
-        for (int i = 0; i < qv->top; i++){
-            a[i] = (qv->mas)[i];
-        }
-    } 
-    else {
-    for (unsigned int i = 0; i < new_capacity; ++i) {
-        a[i] = qv->mas[i];
-    }
-    qv->top = new_capacity;
-    }
-    qv->capacity = new_capacity;
-    delete[] qv->mas;
-    qv->mas = a;
-    return true;
-}
 
 // очистить неиспользуемую память, переехав на новое место с уменьшением capacity до top
 void shrink_to_fit (subvector *qv) {
